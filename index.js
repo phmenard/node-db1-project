e45ec1e0232881b3852517ae8c505a02ae6e43c5
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-19 19:50:45 -0400

    built server

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const server = require("./api/server.js");
+const express = require("express");
+const logger = require("./middleware/logger");
 
+const accountsRouter = require('./accounts/accountsRouter');
+
+//const server = require("./api/server.js");
+
+const server = express();
 const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use("/", accountsRouter);
+
+//server.use(logger);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
